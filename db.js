12c3adceaa6a1fdff0d27b0e4694eb326598f74e
nugen21 <nugen21@gmail.com>
2014-07-15 21:04:21 +0900

    cc

--- db.js
+++ db.js
@@ -2,7 +2,8 @@
 var mongoose = require('mongoose');
 
 // connection uri
-var dbURI = 'mongodb://nugen21:online14@ds027489.mongolab.com:27489/heroku_app27411253';
+var dbURI = 'mongodb://<nugen21>:<online14>@ds027489.mongolab.com:27489/heroku_app27411253';
+//var dbURI = 'mongodb://localhost:27017';
 
 // exports connect function to app.js
 exports.connect = function(){
@@ -17,9 +18,9 @@ exports.connect = function(){
 	});
 	
 	// Failed to connect database
-//	mongoose.connection.on('error', function(err) {
-//		console.log('Failed to get connection in mongoose, err is ' + err);
-//	});
+	mongoose.connection.on('error', function(err) {
+		console.log('Failed to get connection in mongoose, err is ' + err);
+	});
 	
 	// When the connection has disconnected
 	mongoose.connection.on('disconnected', function() {
