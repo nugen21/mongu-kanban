eb492b0ae360fe249123b93348d4fe40cfba78ec
nugen21 <nugen21@gmail.com>
2014-07-15 19:01:01 +0900

    cc

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 
 // connection uri
-var dbURI = 'mongodb://nugen21:online14@kahana.mongohq.com:10004/app27411253';
+var dbURI = 'mongodb://nugen21:online14@ds027489.mongolab.com:27489/heroku_app27411253';
 
 // exports connect function to app.js
 exports.connect = function(){
